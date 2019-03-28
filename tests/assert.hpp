ded57636dde99757a1f967111c3a6cf3002ec549
eidheim <eidheim@gmail.com>
2019-03-28 10:56:16 +0100

    Further improvements to the ASSERT-macro: replaced fprintf with std::cerr and added std::-prefix to abort

--- tests/assert.hpp
+++ tests/assert.hpp
@@ -2,7 +2,8 @@
 #define TESTS_ASSERT_HPP
 
 #include <cstdlib>
+#include <iostream>
 
-#define ASSERT(e) ((void)((e) ? ((void)0) : ((void)fprintf(stderr, "Assertion failed: (%s), function %s, file %s, line %u.\n", #e, __func__, __FILE__, __LINE__), abort())))
+#define ASSERT(e) ((void)((e) ? ((void)0) : ((void)(std::cerr << "Assertion failed: (" << #e << "), function " << __func__ << ", file " << __FILE__ << ", line " << __LINE__ << ".\n"), std::abort())))
 
 #endif /* TESTS_ASSERT_HPP */
