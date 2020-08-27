8a877027a1ac202740f3c276df777ccdc61b92aa
woe <StefanWoe@gmail.com>
2020-08-27 15:08:55 +0200

    Handle "=" in URL query parameters (i.e. site.com?compare=1=2&

--- utility.hpp
+++ utility.hpp
@@ -145,7 +145,8 @@ namespace SimpleWeb {
           name_end_pos = std::string::npos;
           value_pos = std::string::npos;
         }
-        else if(query_string[c] == '=') {
+        else if(query_string[c] == '=' && 
+                name_end_pos == std::string::npos) {
           name_end_pos = c;
           value_pos = c + 1;
         }
